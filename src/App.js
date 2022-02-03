4524f3d19f84aa59b48e0e80aff83d3dc4977182
Lucas Lisboa <lucas@pop-os.localdomain>
2022-02-03 17:32:01 -0300

    requisto 1 deu ruim

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './Login';
+import Search from './Search';
+import Album from './Album';
+import Favorites from './Favorites';
+import Profile from './Profile';
+import ProfileEdit from './ProfileEdit';
+import NotFound from './NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
   }
 }
 
